741e8ee319f74c6a476769ca635814971834dfc7
klang <bukit.klang.83@gmail.com>
2024-02-15 13:38:40 +0800

    use secant method to solve for humn after some input inspection

--- solutions/d21/notes21.md
+++ solutions/d21/notes21.md
@@ -10,3 +10,14 @@
     - exec 
     - store in `seen`
 
+### part two - solving for humn
+
+brute-force seems unfruitful, after checking -60k - 60k. Check whether `humn` affects both or only one value
+
+Only left changes; can we programmatically solve? How about the secant method
+
+- given the value on the right
+- given the op for value on left
+- zero fn: v1 op v2 - vright = 0
+
+The linearity of the equation allows secant to solve it 3 steps
--- solutions/d21/soln.py
+++ solutions/d21/soln.py
@@ -8,6 +8,7 @@ from time import time_ns
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.StreamHandler(sys.stdout))
 
+monkeys = {}
 
 def read_line(fpath: str):
     """Reads the input and yields each line"""
@@ -32,18 +33,43 @@ def find_monkey(monkeys: dict, name: str):
     Assumes the initial 
     """
     # base case - int literal
-    # logger.debug(f'monkey: {name}\tjob: {monkeys[name]}')
+    logger.debug(f'monkey: {name}\tjob: {monkeys[name]}')
     if monkeys[name].isnumeric() or monkeys[name][0] == '-':
-        logger.debug('int; returning')
+        # logger.debug('int; returning')
         return int(monkeys[name])
     # recursive case - search name until int, and return op
     else:
-        logger.debug('job; recursing')
+        # logger.debug('job; recursing')
         mka, op, mkb = monkeys[name].split()
         va = find_monkey(monkeys, mka)
         vb = find_monkey(monkeys, mkb)
+        if name == 'root':
+            logger.debug(f'va: {va}\tvb: {vb}')
         return eval(f'va {op} vb')
 
+def eval_humn(humn: int):
+    """
+    The magic number comes from debug showing the RHS value
+    that we need to make the LHS equal to
+    """
+    monkeys['humn'] = str(humn)
+    return find_monkey(monkeys, 'prrg') - 28379346560301
+
+def secant_method(fn, x0, x1, tol=1e-5):
+    """
+    x - humn
+    fn - traversing the monkey jobs
+    """
+    steps = 0
+    ans = 100
+    while abs(ans) > tol:
+        steps += 1
+        nxt = int(x1 - fn(x1) * (x1 - x0) / (fn(x1) - fn(x0)))
+        x0 = x1
+        x1 = nxt
+        ans = fn(nxt)
+        logger.info(f'step: {steps}\tcurrent: {nxt}')
+    return nxt
 
 def main(sample: bool, part_two: bool, loglevel: str):
     """ """
@@ -56,19 +82,18 @@ def main(sample: bool, part_two: bool, loglevel: str):
     logger.info(f'Using {fp} for {"part 2" if part_two else "part 1"}')
 
     # read input
+    global monkeys
     monkeys = {l[0].strip(): l[1].strip() for line in read_line(fp) if (l := parse_monkey(line))}
     if part_two:
         # modify to equality check
         a, _, b = monkeys['root'].split()
         monkeys['root'] = ' '.join([a, '==', b])
-    logger.debug(f'monkeys: {monkeys}')
+
+    # logger.debug(f'monkeys: {monkeys}')
     # execute
     tstart = time_ns()
     if part_two:
-        humn = 9998
-        while not find_monkey(monkeys, 'root'):
-            humn += 1
-            monkeys['humn'] = str(humn)
+        humn = secant_method(eval_humn, -9999, 9999)
         logger.info(f'humn yells {humn}')
     else:
         root = find_monkey(monkeys, 'root')
